7bf1fd0b8e996122cc7306a8101ee62d762172fa
NikitaDzer <nikita.dzer@mail.ru>
2021-09-21 20:36:47 +0300

    Clean main function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
 #include "solverQE/solverQE.h"
 #include "utils/testQE/readyTestsQE.h"
-#include "utils/utils.h"
 
 int main(void) {
-    // solverQE();
+    solverQE();
     // readyTestsQE();
-    printf("%d", isEqual(1360, 1350));
 	return 0;
 }
 
